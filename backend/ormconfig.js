01c041dabcc3c9b8f381ade040d716887858d6e7
Letícia Moreira Barreto <leticia.barreto@nexadigital.com.br>
2021-10-25 17:49:38 -0300

    entities

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,3 +1,7 @@
+import Users from './dist/modules/users/infra/typeorm/entities/Users'
+import Checklists from './dist/modules/checklists/infra/typeorm/entities/Checklists'
+import Pets from './dist/modules/pets/infra/typeorm/entities/Pets'
+
 module.exports = {
   "type": "postgres",
   "host": process.env.DB_HOST,
@@ -5,7 +9,7 @@ module.exports = {
   "username": process.env.DB_USER,
   "password": process.env.DB_PASS,
   "database": process.env.DB_NAME,
-  "entities": [process.env.DB_ENTITIES],
+  "entities": [Users, Checklists, Pets],
   "migrations": [process.env.DB_MIGRATIONS],
   "cli": {
     "migrationsDir": process.env.DB_MIGRATIONS_DIR
