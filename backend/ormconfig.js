7c6f02dbbcdc806a44ca1de62dfd66e449942c1b
Letícia Moreira Barreto <leticia.barreto@nexadigital.com.br>
2021-10-28 13:26:19 -0300

    changes

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  "entities": ['src/modules/**/infra/typeorm/entities/*.js'],
+  "entities": ['dist/modules/**/infra/typeorm/entities/*.js'],
   "migrations": ['dist/shared/infra/typeorm/migrations/*.js'],
   "cli": {
-    "migrationsDir": ['src/shared/infra/typeorm/migrations/'],
-    "entitiesDir": ['src/modules/**/infra/typeorm/entities']
+    "migrationsDir": ['dist/shared/infra/typeorm/migrations/'],
+    "entitiesDir": ['dist/modules/**/infra/typeorm/entities']
   },
   "ssl": true,
   "dialectOptions": {
