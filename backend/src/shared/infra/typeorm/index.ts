20febec870499fb30a73f8debbc5349dbc12c372
Letícia Moreira Barreto <leticia.barreto@nexadigital.com.br>
2021-11-12 17:44:14 -0300

    create connection

--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -1,11 +1,4 @@
+import "reflect-metadata";
 import { createConnection } from 'typeorm';
 
-createConnection({
-  type: 'postgres',
-  url: 'postgres://ftwhqmngnwkazw:fe19f877eced64efc07f777ed2ef198b831538e5654a68a47131ea52cd0917e5@ec2-52-201-72-91.compute-1.amazonaws.com:5432/dep2o78ptojiem',
-  extra: {
-    ssl: true,
-    rejectUnauthorized: false
-  },
-  entities: ['../../../modules/**/infra/typeorm/entities/*.js']
-});
+export default async () => createConnection();
